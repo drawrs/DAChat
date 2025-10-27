463fc883ed6aa5079ba19b1a47bc7d688fa21a70
Danilo Altheman <daltheman@gmail.com>
2025-10-28 04:33:25 +0700

    Initial Commit

---
+++ DAChat/DAChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DAChatApp.swift
+//  DAChat
+//
+//  Created by Danilo Altheman on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DAChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
